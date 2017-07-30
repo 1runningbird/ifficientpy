2c93e75c59d940a72745929958b8eb8ec4f1ce08
jiangyunfeng <jiangyunfeng@yupiao.com>
2017-07-30 22:46:17 +0800

    lock

--- multithreading/start.py
+++ multithreading/start.py
@@ -4,20 +4,37 @@ import threading
 import time
 
 
-def target():
-    print 'The current threading {} is running'.format(
+num = 0
+lock = threading.Lock()
+def target(s):
+    print 'The current threading {} is running sleep'.format(
         threading.current_thread().name)
-    time.sleep(1)
+    print 'sleep {}'.format(s)
     print 'the current threading {} is ended'.format(
         threading.current_thread().name)
+    global num
+    for i in range(10000):
+        lock.acquire()
+        try:
+            num += s
+            num -= s
+        finally:
+            lock.release()
+    print 'result :', num
 
 print 'the current threading {} is running'.format(
     threading.current_thread().name)
 
-t = threading.Thread(target=target)
+r = threading.Thread(target=target, args=[5])
+s = threading.Thread(target=target, args=[3])
+t = threading.Thread(target=target, args=[2])
 
 t.start()
+s.start()
+r.start()
 t.join()
+s.join()
+r.join()
 print 'the current trreading {} is ended'.format(
     threading.current_thread().name
 )
